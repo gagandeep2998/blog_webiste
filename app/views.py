ec3a3600379c634b5d9abc993d2dbb5de9a867cd
Gagandeep Singh <gagandeepsingh2998@gmail.com>
2023-01-12 14:38:14 +0530

    add show single blog feature

--- app/views.py
+++ app/views.py
@@ -14,6 +14,12 @@ def home():
     return render_template("index.html", posts=all_posts)
 
 
+@views.route("/post/<int:post_id>")
+def show_post(post_id):
+    requested_post = Post.query.get(post_id)
+    return render_template("post.html", post=requested_post)
+
+
 @views.route("/about")
 def about():
     return render_template('about.html')
