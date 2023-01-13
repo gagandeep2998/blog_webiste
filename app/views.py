c115be49a865aaf2ca8357fb47f607d63fdff113
Gagandeep Singh <gagandeepsingh2998@gmail.com>
2023-01-13 13:02:17 +0530

    add new-post route for creating new post

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask import Blueprint
 from .models import Post
 from . import db
-
+from .forms import CreatePostForm
+from datetime import date
 
 views = Blueprint("views", __name__)
 
@@ -18,6 +19,25 @@ def home():
 def show_post(post_id):
     requested_post = Post.query.get(post_id)
     return render_template("post.html", post=requested_post)
+
+
+@views.route("/new-post", methods=["POST", "GET"])
+def add_new_post():
+    form = CreatePostForm()
+    if form.validate_on_submit():
+        new_post = Post(
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            body=form.body.data,
+            img_url=form.img_url.data,
+            author=form.author.data,
+            date=date.today().strftime("%B %d, %Y")
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('views.home'))
+
+    return render_template('make-post.html', form=form)
 
 
 @views.route("/about")
